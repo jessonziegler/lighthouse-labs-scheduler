a7d4bbedac482668cc25f8341caef637fb5a44da
Jesson <jesson@Ziegler-2.local>
2021-11-03 20:40:55 -0400

    cypress test for navigating to tuesday

--- cypress/integration/navigationspec.js
+++ cypress/integration/navigationspec.js
@@ -3,3 +3,10 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 });
+it("should navigate to Tuesday", () => {
+  cy.visit("/");
+
+  cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected");
+});
